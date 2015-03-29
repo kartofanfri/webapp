fd25af6119636cf42b41d0b2906b93c5eef4decd
kartofanfri <kartofanfri@gmail.com>
2015-03-29 09:58:52 +0300

    it's only begin.

--- src/main/java/ru/mai/dep806/mvcapp/dao/MockToDoListDao.java
+++ src/main/java/ru/mai/dep806/mvcapp/dao/MockToDoListDao.java
@@ -12,7 +12,7 @@ public class MockToDoListDao {
     List<ToDoList> toDoList = new ArrayList<ToDoList>();
 
     public MockToDoListDao() {
-        toDoList.add(new ToDoList("some item 1","some item 2","some item 3" ));
+        toDoList.add(new ToDoList("some item 1","some item 2","some item 3 " ));
         toDoList.add(new ToDoList("some item 4","some item 5","some item 6","some item 7"));
     }
 }
